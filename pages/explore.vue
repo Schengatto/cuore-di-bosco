4059178748a55c37ff76fb846ad4de2066c8b53c
Enrico Schintu <enrico.schintu@librecapital.com>
2025-06-25 23:23:34 +0200

    feat(explore): added the page explore

--- pages/explore.vue
+++ pages/explore.vue
@@ -1,6 +1,204 @@
 <template>
   <div>
-    <h1>PAGINA IN COSTRUZIONE</h1>
+    <section class="text-center">
+      <p>A pochi minuti da Brentonico, <strong>Prada</strong> è una piccola frazione di montagna nel cuore del
+        <strong>Parco Naturale del Monte Baldo</strong>, un luogo tranquillo ma facile da raggiungere, ottimo punto
+        per escursioni e attività di ogni genere.
+        Ideale per le famiglie in cerca di natura, silenzio e libertà. Qui i bambini di ogni età (anche quelli
+        cresciuti!) possono correre tra i prati, osservare gli animali al pascolo, ascoltare il canto degli uccelli e
+        vivere l’esperienza della montagna in tranquillità.
+      </p>
+
+      <p>
+        La zona è ricca di <strong>sentieri adatti anche ai più piccoli</strong>, malghe da raggiungere con facili
+        passeggiate, e
+        boschi che cambiano volto a ogni stagione: dal verde brillante della primavera ai colori caldi dell’autunno.
+        In <string>estate</string> si possono scoprire le malghe del Parco Naturale del Monte Baldo, assaggiare i
+        prodotti locali e
+        godersi l’aria fresca d’altura. Sul sito del Parco qui sotto puoi trovare i percorsi e le tante attività in
+        programma!
+      </p>
+
+      <figure class="wp-block-image aligncenter size-large is-resized"><a href="https://www.parcomontebaldo.tn.it/it"
+          target="_blank" rel="noopener noreferrer nofollow"><img decoding="async"
+            src="https://www.parcomontebaldo.tn.it/sites/default/files/logo-parcomontebaldo.jpg" alt=""
+            style="width:132px;height:auto"></a></figure>
+
+      <p>
+        In inverno si raggiungono le <strong>località scistiche</strong> di <strong>Polsa</strong> (10 minuti),
+        <strong>San Giacomo</strong> (15 minuti) e <string>San Valentino</string>
+        (20 minuti), con piste per ogni età e parchi per bambini, ideali per le prime discese sulla neve, imparare a
+        sciare con scuole sci riconosciute o per divertirsi con lo slittino in famiglia, o partecipare alle tante
+        attività di giorno e di notte. Per tutte gli eventi aggiornati e le informazioni puoi visitare
+        <a href="www.brentonicoski.com" target="_blank">www.brentonicoski.com</a>.
+      </p>
+
+      <p>
+        Anche <strong>Rovereto</strong> è vicina a 25 minuti, con i suoi musei, il centro storico e le attività
+        culturali per tutte le
+        età con i suoi rinomati musei tra arte, storia e scienza come il MART e l’innovativo museo di scienza ed
+        archeologia.
+        Per scoprire nel dettaglio <a href="https://www.visitrovereto.it/scopri/territorio/rovereto-citta-di-culture/"
+          target="_blank">Cosa vedere a Rovereto</a>.
+      </p>
+
+      <p>
+        E se volete completare la vostra vacanza con una giornata sul <strong>Lago di Garda</strong>, in circa 30
+        minuti si può
+        raggiungere la cima del Lago di Garda con Torbole e Riva del Garda, con acqua pulita e panorami unici.
+        <a href="https://www.gardatrentino.it/it" target="_blank">Per saperne di più</a>.
+      </p>
+
+      <p>
+        Siamo all’interno della <strong>Vallagarina</strong>: dove natura, cultura e
+        gusto si incontrano. <br>In struttura troverete un ampio display con mappe e materiale informativo e in nostri
+        consigli, per ogni informazione e materiale è possibile visitare il sito <a href="http://www.visitrovereto.it"
+          target="_blank" rel="noopener noreferrer nofollow">www.visitrovereto.it</a> e l’accogliente punto di
+        informazioni turistiche accanto al comune di Brentonico, proprio accanto ad un ampio e curato parco giochi e
+        la piazza alberata principale del paese, <strong>Piazza Cesare Battisti</strong>, dove si svolgono eventi e
+        feste tutto l’anno.
+      </p>
+    </section>
+
+    <section class="text-center">
+      <figure
+        class="wp-block-gallery has-nested-images columns-default is-cropped wp-block-gallery-1 is-layout-flex wp-block-gallery-is-layout-flex">
+        <figure class="wp-block-image size-large">
+          <a href="https://www.visitrovereto.it/" target="_blank" rel="noopener noreferrer nofollow">
+            <img decoding="async" width="850" height="371" data-id="601"
+              src="https://cuoredibosco.altervista.org/wp-content/uploads/2025/04/apt_brentonico.jpg" alt=""
+              class="wp-image-601"
+              srcset="https://cuoredibosco.altervista.org/wp-content/uploads/2025/04/apt_brentonico.jpg 850w, https://cuoredibosco.altervista.org/wp-content/uploads/2025/04/apt_brentonico-320x140.jpg 320w, https://cuoredibosco.altervista.org/wp-content/uploads/2025/04/apt_brentonico-768x335.jpg 768w"
+              sizes="(max-width: 850px) 100vw, 850px"></a>
+        </figure>
+      </figure>
+    </section>
+
+    <section class="text-center">
+      <h3 class="wp-block-heading has-text-align-center">
+        <a href="https://www.visitrovereto.it/wp-content/uploads/2023/12/20230805-brentonico-guida-150x190-ITA-LOW_WEB.pdf"
+          target="_blank" rel="noreferrer noopener">Guida All’Altopiano di Brentonico&nbsp;(PDF – 4,61 MB)</a>
+      </h3>
+    </section>
+
+    <section class="text-center">
+      <h1>Brentonico, il fiore del Baldo</h1>
+
+      <figure class="wp-block-image"><img decoding="async"
+          src="https://www.visitrovereto.it/wp-content/uploads/2019/11/paesaggio-inverno-monte-baldo-foto-valentino-azzolini-1.jpg"
+          alt="paesaggio invernale"></figure>
+      <figure class="wp-block-image"><img decoding="async"
+          src="https://www.visitrovereto.it/wp-content/uploads/2023/07/palazzo-eccheli-baisi-giardino-botanico-brentonico-luca-matassoni-2023-3.jpg"
+          alt="palazzo-eccheli-baisi-giardino-botanico-brentonico-luca-matassoni-2023 (3)"></figure>
+      <figure class="wp-block-image"><img decoding="async"
+          src="https://www.visitrovereto.it/wp-content/uploads/2023/06/Giardino-Botanico-ph-lucamatassoni-web.png"
+          alt="Giardino Botanico-ph-lucamatassoni-web"></figure>
+      <figure class="wp-block-image"><img decoding="async"
+          src="https://www.visitrovereto.it/wp-content/uploads/2019/10/paesaggio-inverno-monte-baldo-foto-tommaso-prugnola-3.jpg"
+          alt="paesaggio inverno"></figure>
+      <figure class="wp-block-image"><img decoding="async"
+          src="https://www.visitrovereto.it/wp-content/uploads/2024/10/Brentonico-il-fiore-del-Baldo-Graziano-Galvagni.jpg"
+          alt="Brentonico, il fiore del Baldo - Graziano Galvagni"></figure>
+      <figure class="wp-block-image"><img decoding="async"
+          src="https://www.visitrovereto.it/wp-content/uploads/2019/11/paesaggio-inverno-monte-baldo-foto-valentino-azzolini-1.jpg"
+          alt="paesaggio invernale"></figure>
+    </section>
+
+    <section class="text-center">
+      <h2>“Giardino d’Italia” nel cuore del Parco del Monte Baldo</h2>
+      <p>È la prima montagna che incontri arrivando in Trentino. Qui puoi fare trekking e mountain bike d’estate e
+        sciare sulle piste di Polsa, San Valentino e San Giacomo d’inverno.</p>
+      <p>Sei sul versante Trentino del Monte Baldo, fra l’Adige ed il Garda, nel cuore del&nbsp;<a
+          href="https://www.visitrovereto.it/scopri/territorio/parco-naturale-locale-del-monte-baldo/" target="_blank"
+          rel="noopener noreferrer nofollow">Parco del Monte Baldo</a>&nbsp;conosciuto dai botanici di tutta Europa come
+        “Giardino d’Italia” per la sua ricchezza di piante e fiori rari.</p>
+      <p>Brentonico è il nome della frazione capoluogo ed il suo comune comprende ulteriori frazioni e località montane:
+        Castione, Cazzano, Crosano, Cornè, Prada, Saccone e Sorne, così come i centri turistici di San Giacomo, San
+        Valentino e Polsa. </p>
+      <p>Al giardino botanico di&nbsp;<a href="http://www.visitrovereto.it/scopri/palazzi/palazzo-eccheli-baisi/"
+          target="_blank" rel="noopener noreferrer nofollow">Palazzo Eccheli-Baisi</a>&nbsp;troverai raccolte molte
+        delle 1.655 piante del Parco del Monte Baldo, la maggioranza di quelle presenti sulle Alpi. Lo apprezzerai al
+        meglio con una visita guidata, che ti illustrerà anche le mostre del palazzo e il&nbsp;<strong>Museo del
+          fossile</strong>.&nbsp;Ascolta il silenzio nell’antica&nbsp;<a
+          href="http://www.visitrovereto.it/scopri/chiese-e-luoghi-sacri/cripta-di-san-giovanni/" target="_blank"
+          rel="noopener noreferrer nofollow">cripta romanica</a>&nbsp;della&nbsp;<a
+          href="http://www.visitrovereto.it/scopri/chiese-e-luoghi-sacri/chiesa-dei-ss-pietro-e-paolo/" target="_blank"
+          rel="noopener noreferrer nofollow">chiesa tardo-medievale dei SS. Pietro e Paolo</a>. Rilassati all’ombra dei
+        secolari ippocastani del parco Cesare Battisti,&nbsp;al centro del paese. In località Sorne, scopri il&nbsp;<a
+          href="http://www.visitrovereto.it/scopri/luoghi-storici/mulino-zeni/" target="_blank"
+          rel="noopener noreferrer nofollow">Mulino Zeni</a>, un mulino dell’Ottocento ancora perfettamente in funzione.
+      </p>
+    </section>
+
+    <section>
+      <h3>In Primavera</h3>
+      <p>Il periodo delle fioriture è perfetto per incontrare e conoscere le piante e i fiori rari che ravvivano
+        l’Altopiano di Brentonico. Percorri i numerosi&nbsp;<strong>itinerari panoramici</strong>&nbsp;scegliendo quello
+        più adatto alle tue capacità, come l’itinerario che parte da Polsa, prosegue fino a Bocca d’Ardole, al Corno
+        della Paura e raggiunge il&nbsp;<a
+          href="https://regio.outdooractive.com/oar-rovereto-trentino/it/tour/trekking/bocca-d-ardole-corno-della-paura-monte-vignola-/13833250/"
+          target="_blank" rel="noreferrer noopener">Monte Vignola</a>, oppure l’escursione nella selvaggia&nbsp;<a
+          href="https://regio.outdooractive.com/oar-rovereto-trentino/it/tour/trekking/escursione-alla-val-del-parol/13835058/#dmdtab=oax-tab5"
+          target="_blank" rel="noreferrer noopener">Val del Parol</a>&nbsp;per ammirarne le preziose fioriture e gli
+        animali all’alpeggio.</p>
+    </section>
+
+    <section>
+      <h3>In Estate</h3>
+      <p>L’estate sul Monte Baldo significa&nbsp;<strong>attività all’aria aperta</strong>!</p>
+      <p>Sei un escursionista, un biker o un amante delle passeggiate a cavallo? Esplora l’estesa rete di strade
+        forestali e sentieri, alcuni a picco sul Lago di Garda da cui potrai godere di una vista mozzafiato, altri sul
+        Sentiero della Pace attraverso i luoghi della Grande Guerra come trincee, gallerie e fortificazioni! Di tanto in
+        tanto fai una sosta in una delle molte&nbsp;<strong>malghe</strong>, da dove potrai ammirare il sorgere del sole
+        assaporando del latte appena munto e deliziosi formaggi.</p>
+      <p>L’altopiano di Brentonico è inoltre sede di&nbsp;<a
+          href="https://www.visitrovereto.it/vivi/outdoor/summer-camp/" target="_blank" rel="noreferrer noopener">camp
+          estivi</a>&nbsp;per ragazzi e punto di riferimento per la preparazione atletica di squadre di calcio
+        professionistiche e delle nazionali azzurre e tedesche di sci.</p>
+      <p>In agosto non perderti il festival&nbsp;<a
+          href="https://www.visitrovereto.it/vivi/eventi/ferragosto-baldo-bio-benessere/" target="_blank"
+          rel="noopener noreferrer nofollow">Baldo Bio Benessere</a>&nbsp;sul Parco C.Battisti, un evento dedicata al
+        relax, al benessere e ai prodotti naturali.</p>
+    </section>
+
+    <section>
+      <h3>In Autunno</h3>
+      <p>Passeggia fra i maestosi alberi dell’altopiano e scopri i caldi colori e i deliziosi sapori dell’autunno. In
+        questa stagione le famose&nbsp;<strong>castagne di Castione</strong>&nbsp;diventano le protagoniste indiscusse
+        degli eventi locali, come la&nbsp;<a
+          href="https://www.visitrovereto.it/vivi/eventi/festa-della-castagna-castione/" target="_blank"
+          rel="noopener noreferrer nofollow">Festa del Marrone</a>, la&nbsp;<a
+          href="https://www.visitrovereto.it/vivi/eventi/luppoli-e-castagne/" target="_blank"
+          rel="noopener noreferrer nofollow">Festa della castagna</a>&nbsp;o&nbsp;<a
+          href="https://www.visitrovereto.it/vivi/eventi/sapori-dautunno/" target="_blank"
+          rel="noopener noreferrer nofollow">Sapori d’Autunno</a>&nbsp;e dei menù autunnali in abbinamento ad altri
+        piatti tipici della tradizione trentina: canederli, strangolapreti, polenta di patate, che potrai accompagnare
+        con gustosi formaggi, strudel e zelten.</p>
+    </section>
+
+    <section>
+      <h3>In Inverno</h3>
+      <p>A&nbsp;<a href="https://www.visitrovereto.it/vivi/outdoor/neve/sci/sci-alpino/" target="_blank"
+          rel="noopener noreferrer nofollow">Polsa e a San Valentino</a>&nbsp;le piste del Trentino più vicine alla
+        pianura: potrai sciare in totale sicurezza e tranquillità su piste ampie e soleggiate. Lo sci non è il tuo
+        sport? Scegli fra altre&nbsp;<a href="https://www.visitrovereto.it/info/guide/inverno-sul-monte-baldo/"
+          target="_blank" rel="noopener noreferrer nofollow"><strong>attività dell’inverno</strong></a>: ciaspole,
+        pattinaggio sul ghiaccio, parco giochi sulla neve o slittino! Imperdibile la fiaccolata notturna e gli eventi
+        sulla neve per tutti!</p>
+      <p>Se vieni a trovarci per le festività natalizie, non perderti il&nbsp;<a
+          href="https://www.visitrovereto.it/vivi/eventi/natale-dell-albero-brentonico/" target="_blank"
+          rel="noopener noreferrer nofollow">Natale dell’Albero</a>: da dicembre all’epifania tante attività per grandi
+        e piccini e l’imperdibile Simposio di scultura nel centro storico del paese.</p>
+    </section>
+
+    <section class="text-center footer">
+      <p><strong>Per scoprire tutti gli eventi in corso ti consigliamo il sito
+          <a href="www.visitrovereto.it" target="_blank">www.visitrovereto.it</a> e l’<a
+            href="https://www.visitrovereto.it/info/uffici-turistici-rovereto-vallagarina/">apt di Brentonico</a>,
+          inoltre troverai un display in struttura con mappe e depliant
+          aggiornati della zona a tua disposizione!</strong></p>
+      <p><strong>FONTE TESTO <a href="www.visitrovereto.it" target="_blank">WWW.VISITROVERETO.IT</a></strong></p>
+    </section>
   </div>
 </template>
 
@@ -8,6 +206,22 @@
 
 </script>
 
-<style>
+<style scoped>
+section {
+  margin: 1em auto;
+  max-width: 1200px;
+  padding: 2rem;
+  font-size: 16pt;
+  text-rendering: optimizeLegibility;
+  line-height: 1.75;
+  font-weight: 400;
+}
 
+a {
+  font-weight: bold;
+}
+
+.footer {
+  max-width: 800px;
+}
 </style>
