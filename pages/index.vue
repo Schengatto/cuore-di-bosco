3b9de979c1cc72484b20de5e628e0c5531528837
Enrico Schintu <schintu.enrico@gmail.com>
2025-07-05 11:26:23 +0200

    fix: migliorata esposizione mappa google

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,20 @@
   <div class="container">
     <div>
       <div class="welcome">
-        <img decoding="async" width="1400" height="800" class="landing-image" alt="panorama" :src="brentonico"
-          data-object-fit="cover" loading="lazy" /><span aria-hidden="true"
+        <img
+          decoding="async"
+          width="1400"
+          height="800"
+          class="landing-image"
+          alt="panorama"
+          :src="brentonico"
+          data-object-fit="cover"
+          loading="lazy"
+        /><span
+          aria-hidden="true"
           class="wp-block-cover__background has-background-dim-30 has-background-dim"
-          style="background-color: #526a71"></span>
+          style="background-color: #526a71"
+        ></span>
         <div class="landing-text">
           <h1 class="landing-title">
             Ritrova il tuo tempo,<br />
@@ -22,14 +32,20 @@
         </p>
 
         <figure class="separator-wrapper">
-          <img decoding="async" class="separator" :src="separator" alt="separator" />
+          <img
+            decoding="async"
+            class="separator"
+            :src="separator"
+            alt="separator"
+          />
         </figure>
 
         <p>
           Benvenuti al <strong>B&B Cuore di Bosco</strong>,<br />
           un rifugio immerso nella natura della
           <strong>Vallagarina</strong>,<br />
-          sull’<strong>Altopiano di Brentonico, il giardino d’Europa</strong>.<br />
+          sull’<strong>Altopiano di Brentonico, il giardino d’Europa</strong
+          >.<br />
           Circondati da rigogliose montagne, il tempo rallenta<br />
           e la tranquillità regna sovrana.
         </p>
@@ -39,59 +55,112 @@
           storia di semplicità e amore per la montagna.
         </p>
         <div>
-          <Button label="PRENOTA ORA IL TUO SOGGIORNO!" :onClick="goToBooking" />
+          <Button
+            label="PRENOTA ORA IL TUO SOGGIORNO!"
+            :onClick="goToBooking"
+          />
         </div>
       </section>
 
       <section class="text-center">
-        <p><em>Cuore di Bosco</em> si trova a Prada di Brentonico,
-          facilmente raggiungibile in auto dall’<b>uscita di Rovereto dell’autostrada A22</b>. </p>
-        <p>Vi consigliamo di passare da <b>Mori</b> e salire seguendo le indicazioni per l'<b>Altopiano di Brentonico</b> (cartelli marroni).
-          Da Brentonico la SP3 vi porterà in pochi minuti a Prada, fate attenzione al bivio per Prada sulla sinistra in curva.
-          Parcheggiate nel parcheggio libero accanto alla chiesa e al parco giochi, siamo a pochi passi sulla sinistra! </p>
+        <p>
+          <em>Cuore di Bosco</em> si trova a Prada di Brentonico, facilmente
+          raggiungibile in auto dall’<b
+            >uscita di Rovereto dell’autostrada A22</b
+          >.
+        </p>
+        <p>
+          Vi consigliamo di passare da <b>Mori</b> e salire seguendo le
+          indicazioni per l'<b>Altopiano di Brentonico</b> (cartelli marroni).
+          Da Brentonico la SP3 vi porterà in pochi minuti a Prada, fate
+          attenzione al bivio per Prada sulla sinistra in curva. Parcheggiate
+          nel parcheggio libero accanto alla chiesa e al parco giochi, siamo a
+          pochi passi sulla sinistra!
+        </p>
         <p>Se hai bisogno di aiuto chiamaci e arriviamo!</p>
-        <p>Una volta arrivati, vi accoglierà il silenzio e la bellezza
-          dell’Altopiano di Brentonico, con la sua cornice di verdi montagne.</p>
+        <p>
+          Una volta arrivati, vi accoglierà il silenzio e la bellezza
+          dell’Altopiano di Brentonico, con la sua cornice di verdi montagne.
+        </p>
       </section>
 
       <div class="text-center">
         <figure class="separator-wrapper">
-          <img decoding="async" class="separator" :src="separator" alt="separator" />
+          <img
+            decoding="async"
+            class="separator"
+            :src="separator"
+            alt="separator"
+          />
         </figure>
       </div>
 
       <section class="text-center">
-        <h2 class="wp-block-heading has-text-align-center">Trovaci con Maps!</h2>
-        <p style="font-family: 'Cardo', serif; font-size: 24px; text-align: center;">Scrivi su maps:<br> B&amp;B Cuore
-          di
-          Bosco <br>
-          Via Gardesani, 15 – Prada (TN)<br>
-          o clicca sulla mappa per aprirla in Google Maps<br> e ottenere il percorso.</p>
-        <a href="https://www.google.com/maps/place/Via+Gardesani,+15,+38060+Prada+TN" target="_blank"
-          rel="noopener noreferrer nofollow" style="display: block; margin-bottom: 10px; font-family: 'Cardo', serif;">
+        <h2 class="wp-block-heading has-text-align-center">
+          Trovaci con Maps!
+        </h2>
+        <p
+          style="
+            font-family: 'Cardo', serif;
+            font-size: 24px;
+            text-align: center;
+          "
+        >
+          Scrivi su maps:<br />
+          B&amp;B Cuore di Bosco <br />
+          Via Gardesani, 15 – Prada (TN)<br />
+          o clicca sulla mappa per aprirla in Google Maps<br />
+          e ottenere il percorso.
+        </p>
+        <a
+          href="https://www.google.com/maps/place/Via+Gardesani,+15,+38060+Prada+TN"
+          target="_blank"
+          rel="noopener noreferrer nofollow"
+          style="
+            display: block;
+            margin-bottom: 10px;
+            font-family: 'Cardo', serif;
+          "
+        >
           <iframe
+            class="map"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4628.566893163929!2d10.95615487755049!3d45.80152017108149!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4782051cad11ec19%3A0x135a8894e1f97c2d!2sVia%20Gardesani%2C%2015%2C%2038060%20Prada%20TN!5e1!3m2!1sit!2sit!4v1749392711552!5m2!1sit!2sit"
-            width="100%" height="500" style="border:0;" allowfullscreen="true" loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade">
+            width="100%"
+            height="300"
+            style="border: 0"
+            allowfullscreen="true"
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          >
           </iframe>
         </a>
       </section>
 
       <section class="text-center">
-        <h2 class="wp-block-heading has-text-align-center">Hai bisogno di contattarci?</h2>
-        <p class="has-text-align-center">Siamo a tua disposizione! Puoi scriverci, chiamarci o inviarci un messaggio su
-          WhatsApp.</p>
+        <h2 class="wp-block-heading has-text-align-center">
+          Hai bisogno di contattarci?
+        </h2>
+        <p class="has-text-align-center">
+          Siamo a tua disposizione! Puoi scriverci, chiamarci o inviarci un
+          messaggio su WhatsApp.
+        </p>
         <div class="row-buttons">
           <p>
             Contattaci al <a href="tel:+393494960228">+39 349 496 0228</a><br />
-            oppure scrivici a <a href="mailto:info@cuoredibosco.it">info@cuoredibosco.it</a>
+            oppure scrivici a
+            <a href="mailto:info@cuoredibosco.it">info@cuoredibosco.it</a>
           </p>
         </div>
       </section>
 
       <section class="breakfast">
         <figure class="separator-wrapper">
-          <img decoding="async" class="separator" :src="separator" alt="separator" />
+          <img
+            decoding="async"
+            class="separator"
+            :src="separator"
+            alt="separator"
+          />
         </figure>
         <h2>A colazione da noi</h2>
 
@@ -103,11 +172,16 @@
           giornata col sorriso.
         </p>
         <figure class="separator-wrapper">
-          <img decoding="async" class="image"
-            src="https://cuoredibosco.altervista.org/wp-content/uploads/2025/06/breakfast.png" alt="breakfast" />
+          <img
+            decoding="async"
+            class="image"
+            src="https://cuoredibosco.altervista.org/wp-content/uploads/2025/06/breakfast.png"
+            alt="breakfast"
+          />
         </figure>
         <p>
-          Preferisci il dolce o il salato? Hai intolleranze o preferenze alimentari?<br />
+          Preferisci il dolce o il salato? Hai intolleranze o preferenze
+          alimentari?<br />
           Basta farcelo sapere il giorno prima: pensiamo noi a tutto.
         </p>
 
@@ -121,26 +195,33 @@
           Teniamo moltissimo alla qualità dei nostri ingredienti, e per questo
           ci riforniamo il più possibile da produttori locali.
         </p>
-              <figure class="separator-wrapper">
-          <img decoding="async" class="separator" :src="separator" alt="separator" />
+        <figure class="separator-wrapper">
+          <img
+            decoding="async"
+            class="separator"
+            :src="separator"
+            alt="separator"
+          />
         </figure>
         <h2>Tutti i progetti nascono da un sogno</h2>
         <p>
-          La volontà di una vita più serena e meno di fretta per noi e per la nostra famiglia ci ha portato ad un cambio di vita completo,
-           dalla brulicante ed asfaltata pianura padana all’Altopiano di Brentonico.</p> 
-           <p> E’ stato amore a prima vista. Il nostro posto.
-           Noi ci siamo ripresi il nostro tempo lento e la nostra pace, unitevi anche voi!
+          La volontà di una vita più serena e meno di fretta per noi e per la
+          nostra famiglia ci ha portato ad un cambio di vita completo, dalla
+          brulicante ed asfaltata pianura padana all’Altopiano di Brentonico.
         </p>
-       
-</section>
+        <p>
+          E’ stato amore a prima vista. Il nostro posto. Noi ci siamo ripresi il
+          nostro tempo lento e la nostra pace, unitevi anche voi!
+        </p>
+      </section>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useRouter } from "vue-router";
-import brentonico from '~/assets/images/brentonico.webp';
-import separator from '~/assets/images/separator.webp';
+import brentonico from "~/assets/images/brentonico.webp";
+import separator from "~/assets/images/separator.webp";
 
 useHead({
   title: "Cuore di Bosco B&B",
@@ -344,6 +425,10 @@ section {
 
   .separator {
     min-width: 400px;
+  }
+
+  .map {
+    margin: 0 1em; 
   }
 }
 
---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Script per avviare l'ambiente di sviluppo
+echo "Avvio di pnpm run dev..."
+pnpm run dev
