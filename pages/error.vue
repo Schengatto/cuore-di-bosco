c42288461062c2f5f7928dd5b50e69f49833445a
Enrico Schintu <enrico.schintu@librecapital.com>
2025-07-07 08:22:23 +0200

    feat: handled invalid page with redirect to homepage

---
+++ pages/error.vue
@@ -0,0 +1,14 @@
+<template>
+  <div v-if="error.statusCode !== 404">
+    <h1>{{ error.statusCode }}</h1>
+    <p>{{ error.message }}</p>
+  </div>
+</template>
+
+<script setup>
+const error = useError();
+
+if (error.statusCode === 404) {
+  navigateTo('/', { replace: true });
+}
+</script>
